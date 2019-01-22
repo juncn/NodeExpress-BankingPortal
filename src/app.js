13d0e22492042a78134e1855deaecc74ccbc2cf8
Guo, Junming <sjunmg@gmail.com>
2019-01-22 12:51:09 -0600

    extract account route and services route into their own files

--- src/app.js
+++ src/app.js
@@ -2,6 +2,8 @@ const fs = require('fs');
 const path = require('path');
 const express = require('express');
 const { accounts, users, writeJSON } = require('./data');
+const accountRoutes = require('./routes/accounts');
+const servicesRoutes = require('./routes/services');
 const app = express();
 
 app.set('views', path.join(__dirname, 'views'));
@@ -14,40 +16,12 @@ app.get('/', (req, res) => {
   res.render('index', { title: 'Account Summary', accounts: accounts });
 });
 
-
-
 app.get('/profile', (req, res) => {
   res.render('profile', { user: users[0] });
 });
 
-app.get('/transfer', (req, res) => {
-  res.render('transfer');
-});
-
-app.post('/transfer', (req, res) => {
-  // Calculate and set the FROM and TO balance
-  // Convert account data to JSON
-  accounts[req.body.from].balance = accounts[req.body.from].balance - parseInt(req.body.amount);
-  accounts[req.body.to].balance = accounts[req.body.to].balance + parseInt(req.body.amount);
-
-  // Write account data to JSON file
-  writeJSON();
-
-  res.render('transfer', { message: 'Transfer Completed' });
-});
-
-app.get('/payment', (req, res) => {
-  res.render('payment', { account: accounts.credit });
-});
-
-app.post('/payment', (req, res) => {
-  accounts.credit.balance = accounts.credit.balance - parseInt(req.body.amount);
-  accounts.credit.available = accounts.credit.available + parseInt(req.body.amount);
-  writeJSON();
-  res.render('payment', {
-    message: 'Payment Successful',
-    account: accounts.credit
-  });
-});
+// Acount and Services related route
+app.use('/account', accountRoutes);
+app.use('/services', servicesRoutes);
 
 app.listen(3000, () => console.log('PS Project Running on port 3000!'));
---
+++ src/routes/services.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const { accounts, writeJSON } = require('../data');
+
+const router = express.Router();
+
+router.get('/transfer', (req, res) => {
+  res.render('transfer');
+});
+
+router.post('/transfer', (req, res) => {
+  // Calculate and set the FROM and TO balance
+  // Convert account data to JSON
+  accounts[req.body.from].balance =
+    accounts[req.body.from].balance - parseInt(req.body.amount);
+  accounts[req.body.to].balance =
+    accounts[req.body.to].balance + parseInt(req.body.amount);
+
+  // Write account data to JSON file
+  writeJSON();
+
+  res.render('transfer', { message: 'Transfer Completed' });
+});
+
+router.get('/payment', (req, res) => {
+  res.render('payment', { account: accounts.credit });
+});
+
+router.post('/payment', (req, res) => {
+  accounts.credit.balance = accounts.credit.balance - parseInt(req.body.amount);
+  accounts.credit.available =
+    accounts.credit.available + parseInt(req.body.amount);
+  writeJSON();
+  res.render('payment', {
+    message: 'Payment Successful',
+    account: accounts.credit
+  });
+});
+
+module.exports = router;
