983b7a8a2ad3e0de14c768f3d83c5072ff6e68f4
Guo, Junming <sjunmg@gmail.com>
2019-01-22 10:33:15 -0600

    update index route and view to contain accounts information

--- src/app.js
+++ src/app.js
@@ -7,9 +7,17 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Read account data
+const accountData = fs.readFileSync('src/json/accounts.json', 'utf8');
+const accounts = JSON.parse(accountData);
+
+// Read user data
+const userData = fs.readFileSync('src/json/users.json', 'utf8');
+const users = JSON.parse(userData);
+
 // Index route
 app.get('/', (req, res) => {
-  res.render('index', { title: 'Index' });
+  res.render('index', { title: 'Account Summary', accounts: accounts });
 });
 
 app.listen(3000, () => console.log('PS Project Running on port 3000!'));
