0873a94de4dfe525bf154396e6c70776a3aecb89
Guo, Junming <sjunmg@gmail.com>
2019-01-22 10:58:19 -0600

    add Express middleware to handle post data

--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ const app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({extended: true}));
 
 // Read account data
 const accountData = fs.readFileSync('src/json/accounts.json', 'utf8');
