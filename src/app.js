620aefe0e683585493a6b182ef5421e941c968fe
Guo, Junming <sjunmg@gmail.com>
2019-01-22 11:05:24 -0600

    update the Transfer view

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ const app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
 // Read account data
 const accountData = fs.readFileSync('src/json/accounts.json', 'utf8');
@@ -35,6 +35,10 @@ app.get('/credit', (req, res) => {
 
 app.get('/profile', (req, res) => {
   res.render('profile', { user: users[0] });
+});
+
+app.get('/transfer', (req, res) => {
+  res.render('transfer');
 });
 
 app.listen(3000, () => console.log('PS Project Running on port 3000!'));
